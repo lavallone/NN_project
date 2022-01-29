273c7baf679efc1b232c49ba00aeb365d059a580
leo <lavalle.18384929@studenti.uniroma1.it>
2022-01-29 22:31:34 +0100

    first commit

---
+++ README 2.md
@@ -0,0 +1,88 @@
+# Mask-invariant Face Recognition through Template-level Knowledge Distillation
+
+This is the official repository of "Mask-invariant Face Recognition through Template-level Knowledge Distillation" accepted at *IEEE International Conference on Automatic Face and Gesture Recognition 2021 (FG2021)*.
+
+<img src="MaskInv_Overview.png"> 
+
+
+Research Paper at:
+
+* [Arxiv](https://arxiv.org/abs/2112.05646)
+* [IEEE Xplore](https://ieeexplore.ieee.org/document/9667081)
+
+## Table of Contents 
+
+- [Abstract](#abstract)
+- [Data](#data)
+- [Model Training](#model-training)
+- [Citing](#citing)
+- [Acknowledgement](#acknowledgement)
+- [License](#license)
+
+### Abstract ###
+
+The emergence of the global COVID-19 pandemic poses new challenges for biometrics. Not only are contactless
+biometric identification options becoming more important, but face recognition has also recently been confronted with the
+frequent wearing of masks. These masks affect the performance of previous face recognition systems, as they hide important identity information. In this paper, we propose a
+mask-invariant face recognition solution (MaskInv) that utilizes template-level knowledge distillation within a training paradigm
+that aims at producing embeddings of masked faces that are similar to those of non-masked faces of the same identities.
+In addition to the distilled knowledge, the student network benefits from additional guidance by margin-based identity
+classification loss, ElasticFace, using masked and non-masked faces. In a step-wise ablation study on two real masked
+face databases and five mainstream databases with synthetic masks, we prove the rationalization of our MaskInv approach.
+Our proposed solution outperforms previous state-of-the-art (SOTA) academic solutions in the recent MFRC-21 challenge
+in both scenarios, masked vs masked and masked vs nonmasked, and also outperforms the previous solution on the
+MFR2 dataset. Furthermore, we demonstrate that the proposed model can still perform well on unmasked faces with only a
+minor loss in verification performance.
+
+## Data ## 
+
+### Datasets ###
+The MFR2 dataset can be downloaded [here](https://github.com/aqeelanwar/MaskTheFace).
+
+The preprocessed benchmarks (LFW, CALFW, CPLFW, AgeDB-30, CFP-FP) can be downloaded [here](https://github.com/deepinsight/insightface/tree/master/recognition/_datasets_) as "MS1M-ArcFace (85K ids/5.8M images)". 
+
+The [MFRC-21](https://www.igd.fraunhofer.de/sites/default/files/media/biblio/2021/2021_boutros_mfr_2021.pdf) dataset is not available for the public.
+
+For all the datasets above, please strictly follow the licence distribution.
+
+### Masks ###
+The mask template used to create the synthetic masked data for training and evaluation is [attached](https://github.com/fdbtrs/Masked-Face-Recognition-KD/blob/main/mask_img.png).
+The colors for the synthetic masks on the benchmark can be downloaded [here](https://drive.google.com/drive/folders/1bW48uHfemK21vzy9eOsCuvvg9iT9RbPt?usp=sharing), they have to be placed in the /eval/ directory.
+
+### Model Training ###
+ 1. Download pretrained ElasitcFace model [ElasticFace-Arc Model](https://github.com/fdbtrs/ElasticFace) and copy it to the output folder
+ 2. Download MS1MV2 dataset from [insightface](https://github.com/deepinsight/insightface/tree/master/recognition/_datasets_) on strictly follow the licence distribution
+ 3. Set the config.rec in config/configKD.py to the dataset path
+ 4. Intall the requirement from requirement.txt: pip install -r requirements.txt
+ 5. run train_kd.py 
+### Pretrained Models ###
+All evaluated pre-trained models are available:
+* [ArcFace Model](https://github.com/deepinsight/insightface/tree/master/model_zoo)
+* [MagFace Model](https://github.com/IrvingMeng/MagFace)
+* [ElasticFace-Arc Model](https://github.com/fdbtrs/ElasticFace)
+
+
+Our models can be downloaded [here](https://drive.google.com/drive/folders/1HMpNlECooNJ3d-on8KGTBEuxUjWnrT1I?usp=sharing).
+
+## Citing ##
+If you use any of the code provided in this repository or the models provided, please cite the following paper:
+```
+@INPROCEEDINGS{huber2021maskinvariant,  
+   author={Huber, Marco and Boutros, Fadi and Kirchbuchner, Florian and Damer, Naser},  
+   booktitle={2021 16th IEEE International Conference on Automatic Face and Gesture Recognition (FG 2021)},   
+   title={Mask-invariant Face Recognition through Template-level Knowledge Distillation},   
+   year={2021},  
+   volume={},  
+   number={},  
+   pages={1-8},  
+   doi={10.1109/FG52635.2021.9667081}
+}
+```
+
+## Acknowledgement ##
+This research work has been funded by the German Federal Ministry of Education and Research and the Hessian Ministry of Higher Education, Research, Science and the Arts within their joint support of the National Research Center for Applied Cybersecurity ATHENE.
+
+## License ##
+
+This project is licensed under the terms of the Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) license.
+Copyright (c) 2021 Fraunhofer Institute for Computer Graphics Research IGD Darmstadt
---
+++ utils/dataset.py
@@ -0,0 +1,201 @@
+import numbers
+import os
+import queue as Queue
+import threading
+import random
+
+import cv2
+
+import mxnet as mx
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+
+landmarks = np.array([[30.2946, 51.6963],
+                      [65.5318, 51.5014],
+                      [48.0252, 71.7366],
+                      [33.5493, 92.3655],
+                      [62.7299, 92.2041]
+                      ], dtype=np.float32 )
+
+
+class BackgroundGenerator(threading.Thread):
+    def __init__(self, generator, local_rank, max_prefetch=6):
+        super(BackgroundGenerator, self).__init__()
+        self.queue = Queue.Queue(max_prefetch)
+        self.generator = generator
+        self.local_rank = local_rank
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        torch.cuda.set_device(self.local_rank)
+        for item in self.generator:
+            self.queue.put(item)
+        self.queue.put(None)
+
+    def next(self):
+        next_item = self.queue.get()
+        if next_item is None:
+            raise StopIteration
+        return next_item
+
+    def __next__(self):
+        return self.next()
+
+    def __iter__(self):
+        return self
+
+
+class DataLoaderX(DataLoader):
+    def __init__(self, local_rank, **kwargs):
+        super(DataLoaderX, self).__init__(**kwargs)
+        self.stream = torch.cuda.Stream(local_rank)
+        self.local_rank = local_rank
+
+    def __iter__(self):
+        self.iter = super(DataLoaderX, self).__iter__()
+        self.iter = BackgroundGenerator(self.iter, self.local_rank)
+        self.preload()
+        return self
+
+    def preload(self):
+        self.batch = next(self.iter, None)
+        if self.batch is None:
+            return None
+        with torch.cuda.stream(self.stream):
+            for k in range(len(self.batch)):
+                self.batch[k] = self.batch[k].to(device=self.local_rank,
+                                                 non_blocking=True)
+
+    def __next__(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        batch = self.batch
+        if batch is None:
+            raise StopIteration
+        self.preload()
+        return batch
+
+
+class MXFaceDataset(Dataset):
+    def __init__(self, root_dir, local_rank):
+        super(MXFaceDataset, self).__init__()
+        self.transform = transforms.Compose(
+            [transforms.ToPILImage(),
+             transforms.RandomHorizontalFlip(),
+             transforms.ToTensor(),
+             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+             ])
+        self.root_dir = root_dir
+        self.local_rank = local_rank
+        path_imgrec = os.path.join(root_dir, 'train.rec')
+        path_imgidx = os.path.join(root_dir, 'train.idx')
+        self.imgrec = mx.recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, 'r')
+        s = self.imgrec.read_idx(0)
+        header, _ = mx.recordio.unpack(s)
+        if header.flag > 0:
+            self.header0 = (int(header.label[0]), int(header.label[1]))
+            self.imgidx = np.array(range(1, int(header.label[0])))
+        else:
+            self.imgidx = np.array(list(self.imgrec.keys))
+            
+        self.mask_img = cv2.imread("mask_img.png", cv2.IMREAD_UNCHANGED)
+
+    def __getitem__(self, index):
+        idx = self.imgidx[index]
+        s = self.imgrec.read_idx(idx)
+        header, img = mx.recordio.unpack(s)
+        label = header.label
+        if not isinstance(label, numbers.Number):
+            label = label[0]
+        label = torch.tensor(label, dtype=torch.long)
+        sample = mx.image.imdecode(img).asnumpy()
+        prob=random.uniform(0, 1)
+        if (prob<0.5):
+            masked_sample=self.mask_images(sample)
+            if self.transform is not None:
+             sample = self.transform(sample)
+             masked_sample=self.transform(masked_sample)
+            return masked_sample,sample, label
+        else:
+            if self.transform is not None:
+                sample = self.transform(sample)
+            return sample,sample,label
+    
+    def mask_images(self,img):
+        
+        # get landmarks
+        nose = (landmarks[2][0], landmarks[2][1])
+        mouth_left = (landmarks[4][0], landmarks[4][1])
+        mouth_right = (landmarks[3][0], landmarks[3][1])
+        eye_left = (landmarks[1][0], landmarks[1][1])
+        eye_right = (landmarks[0][0], landmarks[0][1])
+    
+        #apply random shift of fakemask
+        rs = np.random.randint(-40,40)
+        rx = np.random.randint(-10,10)
+    
+        #keypoints of mask image
+        src_pts = np.array([np.array([678+rx,464+rs]), 
+                            np.array([548+rx,614+rs]), 
+                            np.array([991+rx,664+rs]), 
+                            np.array([1009+rx,64+rs]), 
+                            np.array([557+rx,64+rs])], dtype="float32")
+
+        #landmark of image
+        dst_pts= np.array([np.array([int(nose[0]), int(nose[1])]), 
+                           np.array([int(mouth_left[0]), int(mouth_left[1])]), 
+                           np.array([int(mouth_right[0]), int(mouth_right[1])]), 
+                           np.array([int(eye_right[0]), int(eye_right[1])]), 
+                           np.array([int(eye_left[0]), int(eye_left[1])])], dtype='float32')
+
+        # compute perspective transformation matrix. src_pts -> dst_pts
+        # The output matrix is used in next step for the transformation of 
+        # the mask to an output-mask which fits to the landmark of the image
+        M, _ = cv2.findHomography(src_pts, dst_pts)
+    
+        # transform the mask to a mask which fits to the image
+        transformed_mask = cv2.warpPerspective(
+                         self.mask_img,
+                         M,
+                         (img.shape[1], img.shape[0]),
+                         None,
+                         cv2.INTER_LINEAR,
+                         cv2.BORDER_CONSTANT)
+ 
+        # overlay the image with the fitting mask
+        alpha_mask = transformed_mask[:, :, 3] / 255
+        alpha_image = np.abs(1 - alpha_mask)
+        
+        # fix mask values
+        transformed_mask = transformed_mask / 255 * 100
+        
+        # add color to masks
+        transformed_mask = self.cymk_to_rgb(transformed_mask)
+        random_value = np.random.randint(0,150,3)
+        transformed_mask = transformed_mask + random_value
+        
+        for c in range(0, 3):
+            img[:, :, c] = (alpha_mask * transformed_mask[:, :, c] + alpha_image * img[:, :, c])
+
+        return img
+
+    def __len__(self):
+        return len(self.imgidx)
+
+    def cymk_to_rgb(self, img):
+        cyan = img[:,:,0] 
+        magenta = img[:,:,1] 
+        yellow = img[:,:,2] 
+        black = img[:,:,3]
+        
+        scale = 100
+        red = 255*(1.0-(cyan+black)/float(scale))
+        green = 255*(1.0-(magenta+black)/float(scale))
+        blue = 255*(1.0-(yellow+black)/float(scale))
+            
+        rgbimg = np.stack((red, green, blue))
+        rgbimg = np.moveaxis(rgbimg, 0, 2)
+        return rgbimg
+        
