10ee7cd0e049d7cba1d85d19c6224608266dc417
lavallone <lavalle.1838492@studenti.uniroma1.it>
2022-09-08 09:58:10 +0200

    a

--- Code/main_dino.py
+++ Code/main_dino.py
@@ -307,6 +307,9 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
 
         # move images to gpu
         images = [im.cuda(non_blocking=True) for im in images]
+        print("#######################")
+        print(images.shape())
+        print(images)
         # teacher and student forward passes + compute dino loss
         with torch.cuda.amp.autocast(fp16_scaler is not None):
             teacher_output = teacher(images[:2])  # only the 2 global views pass through the teacher
