3e653daebdaa5410412059aefecc58bd03e0d5d5
lavallone <lavalle.1838492@studenti.uniroma1.it>
2022-09-09 16:20:12 +0200

    s

--- Code/utils/visualize_attention.py
+++ Code/utils/visualize_attention.py
@@ -31,8 +31,9 @@ from torchvision import transforms as pth_transforms
 import numpy as np
 from PIL import Image
 
-import architectures.vision_transformer as vits
-import architectures.hubconf as pretrained
+from utils  import utils
+from architectures import vision_transformer as vits
+from architectures import hubconf as pretrained
 
 def apply_mask(image, mask, color, alpha=0.5):
     for c in range(3):
