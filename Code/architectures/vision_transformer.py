8429ba97b2f79664526f24e3179ed45b04550005
lavallone <lavalle.1838492@studenti.uniroma1.it>
2022-09-10 12:46:17 +0200

    s

--- Code/architectures/vision_transformer.py
+++ Code/architectures/vision_transformer.py
@@ -21,8 +21,9 @@ from functools import partial
 import torch
 import torch.nn as nn
 
+# capire  come importare nel migliore dei modi!
 import sys
-sys.path.append("../utils")
+sys.path.append("utils")
 import utils
 
 
