fe414a18dacc04ee00015e2e853d070e88aa8ebe
lavallone <lavalle.1838492@studenti.uniroma1.it>
2022-09-27 21:47:39 +0200

    s

--- Code/attention_image.py
+++ Code/attention_image.py
@@ -202,7 +202,7 @@ if __name__ == '__main__':
     model.to(device)
 
     nh, img, attentions, th_attn = compute_attention(args.image_path, args.image_size, args.patch_size, model, args.threshold)
-    print(attentions.shape())
+    print(len(attentions))
     
     # save attentions heatmaps --> we save "nh" attention map images
     os.makedirs(args.output_dir, exist_ok=True)
